dd11e0989e156beac892eae9a897673d5ae1c4d5
Michael <mbane@omnisync.io>
2024-03-20 08:23:17 -0700

    Update btn dark mode UI

--- components/shared/button/index.tsx
+++ components/shared/button/index.tsx
@@ -20,7 +20,7 @@ export const Button = (props: ButtonProps) => {
     <button
       disabled={disabled}
       onClick={loading ? null : onClick}
-      className={`mt-2 py-2 w-full flex flex-row gap-2 align-middle justify-center bg-slate-200 dark:bg-slate-900 p-1 rounded-sm hover:shadow-sm uppercase ${loading ? "animate-pulse" : ""}`}>
+      className={`mt-2 py-2 w-full flex flex-row gap-2 align-middle justify-center bg-slate-200 dark:bg-slate-900 dark:hover:text-yellow-500 p-1 rounded-sm hover:shadow-sm uppercase ${loading ? "animate-pulse" : ""}`}>
       {initialIcon && initialIcon}
       {text}
     </button>
--- components/shared/copy-button/index.tsx
+++ components/shared/copy-button/index.tsx
@@ -12,8 +12,12 @@ export const CopyButton = memo((props: CopyButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`mt-2 py-2 w-full flex flex-row gap-2 align-middle justify-center bg-slate-200 dark:bg-slate-900 p-1 rounded-sm ${hasCopied ? "animate-pulse" : ""} hover:shadow-sm`}>
-      {hasCopied ? <ClipboardCheck size={14} /> : <Clipboard size={14} />}
+      className={`mt-2 py-2 w-full flex flex-row gap-2 align-middle justify-center bg-slate-200 dark:bg-slate-900 p-1 rounded-sm ${hasCopied ? "animate-pulse" : ""} hover:shadow-sm dark:hover:text-yellow-500`}>
+      {hasCopied ? (
+        <ClipboardCheck size={14} className="dark:text-red-500" />
+      ) : (
+        <Clipboard size={14} />
+      )}
     </button>
   )
 })
